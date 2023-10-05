ce9af71af7a2450029b683209c443b9a3ff73d94
Mitesh savaliya <msavaliya@plusonex.com>
2023-10-05 14:33:56 +0530

    connection to db

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import connectToDb from "./connectDb/connectionDb";
+
 const app = express();
 
 import cors from "cors";
+import router from "./routes";
+connectToDb();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
 
 const server = (app: express.Application) => {
-  app.listen(3000, () => {
+  app.listen(8000, () => {
     console.log("Server is running on port 3000");
   });
 }
