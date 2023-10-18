8aa2af0cfe4101e8bd1b2ec2a7660995cfbb3ff7
Mitesh savaliya <msavaliya@plusonex.com>
2023-10-18 16:16:54 +0530

    by query solved

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -3,6 +3,6 @@ const categoryRouter = express.Router();
 import { createCategory, getAllCategories, getCategory, updateCategory, deleteCategory } from "../controller/categoryController";
 import authMiddleware from "../middleware/authMiddleware";
 categoryRouter.route("/").get(getAllCategories).post(authMiddleware, createCategory);
-categoryRouter.route("/getCategory").post(getCategory);
+categoryRouter.route("/getCategory").get(getCategory);
 categoryRouter.route("/:_id").put(authMiddleware,updateCategory).delete(authMiddleware,deleteCategory);
 export default categoryRouter;
